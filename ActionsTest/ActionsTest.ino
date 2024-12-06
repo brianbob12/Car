0d1fb74abd6c8016a37a3a271896c9d53e8a3e7d
Cyrus Singer <japaneserhino@gmail.com>
2024-12-06 00:26:48 -0500

    progress

--- ActionsTest/ActionsTest.ino
+++ ActionsTest/ActionsTest.ino
@@ -8,12 +8,6 @@ void setup(){
   setup_Actions();
 
   addAction(forward_one_cell);
-  addAction(turn_left_30_degrees);
-  addAction(forward_one_cell);
-  addAction(turn_right_90_degrees);
-  addAction(forward_one_cell);
-  addAction(turn_right_30_degrees);
-  addAction(forward_one_cell);
 }
 
 void loop(){
